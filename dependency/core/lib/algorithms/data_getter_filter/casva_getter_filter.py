83e070cea1d2dcb6662f4cdbd0758ed4b67b9a78
onecheck <1548767361@qq.com>
2024-12-09 09:35:27 +0800

    feat: update baseline: casva

--- dependency/core/lib/algorithms/data_getter_filter/casva_getter_filter.py
+++ dependency/core/lib/algorithms/data_getter_filter/casva_getter_filter.py
@@ -11,18 +11,18 @@ __all__ = ('CASVADataGetterFilter',)
 @ClassFactory.register(ClassType.GEN_GETTER_FILTER, alias='casva')
 class CASVADataGetterFilter(BaseDataGetterFilter, abc.ABC):
 
-    def __init__(self):
+    def __init__(self, data_coming_interval: float = 4):
         self.skip_count = 0
         self.latest_time = 0
+        self.data_coming_interval = data_coming_interval
 
     def reset_filter(self):
         self.skip_count = 0
 
     def __call__(self, system):
-        data_coming_interval = 2
         record_time = time.time()
-        if self.latest_time > 0 and record_time - self.latest_time > data_coming_interval:
-            LOGGER.debug(f'[Getter Filter] delay time: {record_time-self.latest_time}')
+        if self.latest_time > 0 and record_time - self.latest_time > self.data_coming_interval:
+            LOGGER.debug(f'[Getter Filter] delay time: {record_time - self.latest_time}')
             self.skip_count += 1
             self.latest_time = record_time
             return False
