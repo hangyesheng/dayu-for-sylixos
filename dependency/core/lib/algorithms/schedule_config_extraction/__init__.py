7a72b2f35960437252f23b1ec9371cc5730cae25
onecheck <1548767361@qq.com>
2024-12-09 17:38:31 +0800

    feat: update hei-drl

--- dependency/core/lib/algorithms/schedule_config_extraction/__init__.py
+++ dependency/core/lib/algorithms/schedule_config_extraction/__init__.py
@@ -3,3 +3,4 @@ from . import hei_config_extraction
 from . import casva_config_extraction
 from . import fc_config_extraction
 from . import chameleon_config_extraction
+from . import hei_drl_config_extraction
