e21a488cba7fc56a5a905195477d791785f19dca
onecheck <1548767361@qq.com>
2024-12-10 13:51:16 +0800

    feat: update baseline: cevas

--- dependency/core/lib/algorithms/before_submit_task_operation/cevas_operation.py
+++ dependency/core/lib/algorithms/before_submit_task_operation/cevas_operation.py
@@ -1,13 +1,11 @@
 import abc
-import json
 import os
-import shutil
-import subprocess
+
 
 from .base_operation import BaseBSTOperation
 
-from core.lib.common import ClassFactory, ClassType, LOGGER, VideoOps
-from core.lib.content import Task
+from core.lib.common import ClassFactory, ClassType
+
 
 __all__ = ('CEVASBSTOperation',)
 
