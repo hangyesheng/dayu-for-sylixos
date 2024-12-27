fff735d76f629736e3f1324cbe8502ae683e1d97
onecheck <1548767361@qq.com>
2024-12-28 01:51:14 +0800

    feat: update docker build

--- hack/lib/buildx.sh
+++ hack/lib/buildx.sh
@@ -13,7 +13,8 @@ dayu::buildx::read_driver_opts() {
   _driver_opts_array=()
   if [[ -f "$driver_opts_file" ]]; then
     local env_opts
-    env_opts=$(yq -t '.env | to_entries[] | "env.\(.key)=\(.value)"' "$driver_opts_file")
+    env_opts=$(yq eval '.env | to_entries[] | "env.\(.key)=\(.value)"' "$driver_opts_file")
+
     while IFS= read -r line; do
       _driver_opts_array+=( "--driver-opt" "$line" )
     done <<< "$env_opts"
