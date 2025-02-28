c9b6d13b7819da4efc565a9eb0e314fa2a2f9a65
onecheck <1548767361@qq.com>
2025-03-01 06:05:38 +0800

    feat: update dag view in frontend

--- frontend/src/views/dag/DagManage.vue
+++ frontend/src/views/dag/DagManage.vue
@@ -426,8 +426,8 @@ export default {
             });
           })
           .catch((error) => {
-            // console.error('Error fetching data:', error);
-            console.error("Error fetching data");
+            console.error('Error fetching data:', error);
+            // console.error("Error fetching data");
           });
     },
     fetchData() {
