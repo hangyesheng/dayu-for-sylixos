b6dbdbdefba28ac5a703c306139b4a800cf5ad53
onecheck <1548767361@qq.com>
2025-04-07 15:57:08 +0800

    feat: update module import mechanism

--- dependency/core/lib/algorithms/__init__.py
+++ dependency/core/lib/algorithms/__init__.py
@@ -1,21 +1,11 @@
-import importlib
 import pkgutil
+import importlib
 
-def auto_import_submodules(package_name):
-    package = importlib.import_module(package_name)
-    modules = {}
-    if hasattr(package, '__path__'):
-        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-            full_name = f"{package_name}.{name}"
-            sub_module = importlib.import_module(full_name)
-            if hasattr(sub_module, '__all__'):
-                for item in sub_module.__all__:
-                    modules[item] = getattr(sub_module, item)
-            else:
-                modules[name] = sub_module
-    return modules
+__all__ = []
 
-_submodules = auto_import_submodules(__name__)
-globals().update(_submodules)
 
-__all__ = list(_submodules.keys())
+for _, name, is_pkg in pkgutil.iter_modules(__path__):
+    if is_pkg:
+        module = importlib.import_module(f'.{name}', __name__)
+        globals()[name] = module
+        __all__.append(name)
