2d47a0976d3bb0248ee468e2f4fa1a6fda8d6828
onecheck <1548767361@qq.com>
2025-04-16 21:56:37 +0800

    style: simplify frontend

--- frontend/src/layout/lockScreen/index.vue
+++ frontend/src/layout/lockScreen/index.vue
@@ -28,7 +28,7 @@
 				<div v-show="state.isShowLoockLogin" class="layout-lock-screen-login">
 					<div class="layout-lock-screen-login-box">
 						<div class="layout-lock-screen-login-box-img">
-							<img src="@/assets/avatar.jpg" />
+							<img src="@/assets/images/avatar.jpg" />
 						</div>
 						<div class="layout-lock-screen-login-box-name">Administrator</div>
 						<div class="layout-lock-screen-login-box-value">
